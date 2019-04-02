4f7af621f8e2a4a14d49324d0d731c1a15ac94ec
João <joaonunesv40@gmail.com>
2019-04-01 23:03:08 -0300

    Projeto de listas

---
+++ src/main/java/br/senai/sc/listatarefas/resources/TarefaResource.java
@@ -0,0 +1,53 @@
+package br.senai.sc.listatarefas.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.senai.sc.listatarefas.domain.Tarefa;
+import br.senai.sc.listatarefas.repositories.TarefaRepository;
+
+@RestController
+@RequestMapping(value = "/tarefas")
+public class TarefaResource {
+
+	@Autowired
+	private TarefaRepository repo;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Tarefa>> ListarTodos() {
+		List<Tarefa> lista = repo.findAll();
+		return ResponseEntity.ok(lista);
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Tarefa> buscar(@PathVariable Integer id) {
+		Optional<Tarefa> obj = repo.findById(id);
+		return ResponseEntity.ok(obj.orElse(null));
+	}
+
+	// post é um simulador
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+		repo.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Tarefa obj, @PathVariable Integer id) {
+		obj.setId(id);
+		repo.save(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+}
